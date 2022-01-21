adc96da293659a9c820465ed3c3411c99a599103
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2022-01-21 10:18:04 +0100

    Added draft of js library

---
+++ bitcoin_client_js/src/index.ts
@@ -0,0 +1,13 @@
+import AppClient from './lib/appClient';
+
+import { PsbtV2 } from './lib/psbtv2';
+import { WalletPolicy } from './lib/policy';
+
+export {
+    AppClient,
+    PsbtV2,
+    WalletPolicy
+};
+
+export default AppClient;
+
